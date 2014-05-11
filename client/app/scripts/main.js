8b79d76c4ca0baa651b018ec0fd3a5ab93f30220
masahirompp <masahirompp@gmail.com>
2014-05-11 20:12:45 +0900

    formated

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -1,18 +1,19 @@
 require.config({
-    paths: {
-        jquery: '../bower_components/jquery/jquery'
-    },
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
+  paths: {
+    jquery: '../bower_components/jquery/jquery'
+  },
+  shim: {
+    bootstrap: {
+      deps: ['jquery'],
+      exports: 'jquery'
     }
+  }
 });
 
-require(['app', 'jquery'], function (app, $) {
-    'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+require(['app', 'jquery'], function(app, $) {
+  'use strict';
+  // use app here
+  console.log(app);
+  console.log('Running jQuery %s', $()
+    .jquery);
 });
