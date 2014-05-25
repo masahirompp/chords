b22970bca2c92d61e5491f884bee9ba7746838b9
masahirompp <masahirompp@gmail.com>
2014-05-25 18:17:26 +0900

    add server side grunt file.

---
+++ server/Gruntfile.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      jshint: {
+        tasks: ['jshint']
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'app.js',
+        'model.js',
+        'route/*.js'
+      ]
+    }
+  });
+
+  grunt.registerTask('default', 'Log some stuff.', function() {
+    grunt.log.write('Logging some stuff...')
+      .ok();
+
+    grunt.task.run([
+      'jshint',
+      'watch'
+    ]);
+
+  });
+};
