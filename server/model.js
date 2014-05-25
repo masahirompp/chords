18e29a160df7c8dc4277e39b37d639f5a61c67fd
masahirompp <masahirompp@gmail.com>
2014-05-25 17:48:29 +0900

    working

---
+++ server/model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var uri = 'mongodb://localhost/chord';
+var db = mongoose.createConnection(uri, function(err, res) {
+  if (err) {
+    console.log('Error connected: ' + uri + ' - ' + err);
+  } else {
+    console.log('Success connected: ' + uri);
+  }
+});
+
+// define
+var userSchema = new mongoose.Schema({
+  userID: String,
+  name: String,
+  email: String,
+  created: Date,
+  updated: Date
+});
+exports.User = db.model('User', userSchema);
+
+var artistSchema = new mongoose.Schema({
+  artistID: String,
+  name: String,
+  url: String,
+  created: Date,
+  updated: Date
+});
+exports.
--- server/routes/songs.js
+++ server/routes/songs.js
@@ -1,9 +1,23 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.send('songs list');
+});
+router.get('/:id', function(req, res) {
+  res.send('song id is ' + req.params.id);
+});
+router.post('/', function(req, res) {
+  res.send('new song');
+});
+router.put('/:id', function(req, res) {
+  res.send('update ' + req.params.id);
+});
+router.delete('/:id', function(req, res) {
+  res.send('delete ' + req.params.id);
 });
 
 module.exports = router;
