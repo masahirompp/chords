46e4a3c7a004f550d8f92462829e893b0c48a8f1
masahirompp <masahirompp@gmail.com>
2014-08-30 20:03:16 +0900

    remove option

--- server/models/chord.js
+++ server/models/chord.js
@@ -2,24 +2,10 @@
 
 var mongoose = require('mongoose'), Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
-var option = new Schema({
-  staffSpace: { type: Number, default: 20},
-  lineSpace: { type: Number, default: 2},
-  underlineSpace: { type: Number, default: 2},
-  hasPageNo: { type: Boolean, default: true},
-  staffType: { type: String, default: 'staff'},
-  barCount: { type: Number, default: 4},
-  musicalTime: { type: Number, default: 8},
-  hasClef: { type: Boolean, default: true},
-  clef: { type: String, default: 'gClef'},
-  hasKey: { type: Boolean, default: false},
-  hasBarNo: { type: Boolean, default: false}
-});
-
 var Chord = new Schema({
   scoreid: {type: ObjectId, ref: 'Score', require: true},
   chords: {type: Array},
-  option: {type: [option], require: true},
+  option: {type: Schema.Types.Mixed, require: true},
   created: { type: Date, default: Date.now },
   updated: Date
 });
