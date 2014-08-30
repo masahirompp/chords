852844b6aceb75addfff002752351218c5d0a43a
masahirompp <masahirompp@gmail.com>
2014-08-30 20:00:48 +0900

    add option

--- server/models/chord.js
+++ server/models/chord.js
@@ -2,14 +2,29 @@
 
 var mongoose = require('mongoose'), Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
+var option = new Schema({
+  staffSpace: { type: Number, default: 20},
+  lineSpace: { type: Number, default: 2},
+  underlineSpace: { type: Number, default: 2},
+  hasPageNo: { type: Boolean, default: true},
+  staffType: { type: String, default: 'staff'},
+  barCount: { type: Number, default: 4},
+  musicalTime: { type: Number, default: 8},
+  hasClef: { type: Boolean, default: true},
+  clef: { type: String, default: 'gClef'},
+  hasKey: { type: Boolean, default: false},
+  hasBarNo: { type: Boolean, default: false}
+});
+
 var Chord = new Schema({
   scoreid: {type: ObjectId, ref: 'Score', require: true},
   chords: {type: Array},
-  option: {type: Schema.Types.Mixed, require: true},
+  option: {type: [option], require: true},
   created: { type: Date, default: Date.now },
   updated: Date
 });
 
+// 保存前に現在時刻を挿入
 Chord.pre('save', function(next) {
   this.updated = new Date();
   next();
