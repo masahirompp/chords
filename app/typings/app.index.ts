98a887fcdd3295f8999b47a29c4c802b301fa30f
masahirompp <masahirompp@gmail.com>
2014-10-08 01:10:25 +0900

    design update

--- app/typings/app.index.ts
+++ app/typings/app.index.ts
@@ -1,10 +1,12 @@
 /// <reference path="../../typings/requirejs/require.d.ts" />
 /// <reference path="../../typings/jquery/jquery.d.ts" />
+/// <reference path="../../typings/bootstrap/bootstrap.d.ts" />
 
 require.config(<RequireConfig>{baseUrl: '/scripts'});
 
 require([], () => {
   console.log('index');
   console.log('Running jQuery %s', $().jquery);
+  $('.btn').tooltip();
 });
 
