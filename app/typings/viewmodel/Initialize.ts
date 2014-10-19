823e16369eaf85e5c7de7c741352d07e39014253
masahirompp <masahirompp@gmail.com>
2014-10-19 19:04:32 +0900

    block

--- app/typings/viewmodel/Initialize.ts
+++ app/typings/viewmodel/Initialize.ts
@@ -2,11 +2,12 @@
 /// <reference path="../../../tsd/blockui/jquery.blockUI.d.ts" />
 /// <reference path="../../../tsd/ext/ext.d.ts" />
 
-(($)=> {
+(($) => {
   $.blockUIl = () => {
     $.blockUI({
-      message: '<img src="/images/loading.gif" /> ',
-      baseZ: 2000
+      message: null,
+      baseZ: 2000,
+      fadeIn: 0
     });
   };
 })($);
