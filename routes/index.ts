8a12f85d456d0e3f82b5d1585bca8540a0d821d0
masahirompp <masahirompp@gmail.com>
2014-11-05 23:59:12 +0900

    ChordKitchen!!

--- routes/index.ts
+++ routes/index.ts
@@ -7,7 +7,7 @@ var router: express.Router = express.Router();
 /* GET home page. */
 router.get('/', (req: express.Request, res: express.Response) => {
   res.render('index', {
-    title: 'コード譜共有サイト ChordsCliche',
+    title: 'コード譜共有サイト ChordKitchen',
     keyword: ''
   });
 });
@@ -15,14 +15,14 @@ router.get('/', (req: express.Request, res: express.Response) => {
 router.get('/:artist/:song/:id', (req: express.Request, res: express.Response) => {
   // TODO 存在チェック
   res.render('score', {
-    title: req.params.artist + ' ' + req.params.song + ' (' + req.params.id + ') | ChordsCliche',
+    title: req.params.artist + ' ' + req.params.song + ' (' + req.params.id + ') | ChordKitchen',
     keyword: ''
   });
 });
 
 router.get('/search', (req: express.Request, res: express.Response) => {
   res.render('search', {
-    title: req.query.q + 'の検索結果 | ChordsCliche',
+    title: req.query.q + 'の検索結果 | ChordKitchen',
     keyword: ''
   });
 });
