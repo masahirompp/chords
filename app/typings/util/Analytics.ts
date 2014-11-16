8f94a6c68c34968f318cad8de13491da5703f4e9
masahirompp <masahirompp@gmail.com>
2014-11-16 17:37:46 +0900

    ga

--- app/typings/util/Analytics.ts
+++ app/typings/util/Analytics.ts
@@ -1,16 +1,17 @@
 /// <reference path="../../../tsd/ga/ga.d.ts" />
 
-((b, o, i, l, e ? , r ? ) => {
-  b.GoogleAnalyticsObject = l;
-  b[l] || (b[l] = function() {
-    (b[l].q = b[l].q || [])
+((i, s, o, g, r, a ? , m ? ) => {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || [])
     .push(arguments)
-  });
-  b[l].l = +new Date;
-  e = o.createElement(i);
-  r = o.getElementsByTagName(i)[0];
-  e.src = '//www.google-analytics.com/analytics.js';
-  r.parentNode.insertBefore(e, r)
-})(window, document, 'script', 'ga');
-ga('create', 'UA-XXXXX-X');
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-55221400-3', 'auto');
 ga('send', 'pageview');
