b02ce69c5f9c05b4d9db7343703b76a6b3f7946e
masahirompp <masahirompp@gmail.com>
2014-12-04 20:23:21 +0900

    es6promise add

--- tsd/tsd.d.ts
+++ tsd/tsd.d.ts
@@ -4,3 +4,4 @@
 /// <reference path="q/Q.d.ts" />
 /// <reference path="passport/passport.d.ts" />
 /// <reference path="express-session/express-session.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
